b65ac41997c2277a720b5d8fd0750666549d949a
Som <94239450+fortheloveofcoding99@users.noreply.github.com>
2022-05-20 01:53:30 +0530

    Add files via upload
    added on May 22nd

---
+++ Practice2.py
@@ -0,0 +1,53 @@
+
+# Area of a triangle
+import math
+
+# bre = float(input("Enter base length:"))
+# hei = float(input("Enter height:"))
+# un = input("enter unit:")
+#
+# def area_triangle(b, h, u):
+#     area = 0.5*b*h
+#     print("Area is",area,"sq.",u)
+#
+# area_triangle(bre,hei,un)
+
+# GCD of 2 numbers
+
+# first_num = int(input("x:"))
+# second_num = int(input("y:"))
+#
+# def gcd_nos(x, y):
+#     if (x==y):
+#         print("GCD is :",x)
+#     elif (x==0):
+#         print("GCD is :",y)
+#     elif (y==0):
+#         print("GCD is :",x)
+#     elif(x>y):
+#         print(math.gcd(x-y, y))
+#     else:
+#         print(math.gcd(y, y-x))
+#
+# gcd_nos(first_num, second_num)
+
+# lcm of 2 numbers
+#
+# first_num = int(input("x:"))
+# second_num = int(input("y:"))
+#
+#
+# def lcm_numbers(x, y):
+#     if (x>y):
+#         z = x
+#     else:
+#         z = y
+#     etr = 1
+#     while(True):
+#         if(z*etr)%x==0 and (z*etr)%y==0:
+#             LCM = etr*z
+#             break
+#         etr += 1
+#     print(LCM)
+#
+# lcm_numbers(first_num, second_num)
