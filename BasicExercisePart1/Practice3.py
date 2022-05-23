518a4700fcaed713b002b5959d1d57bd601ba5ec
Som <94239450+fortheloveofcoding99@users.noreply.github.com>
2022-05-23 22:23:14 +0530

    Add files via upload

---
+++ BasicExercisePart1/Practice3.py
@@ -0,0 +1,57 @@
+
+# #A Python program to sum of three given integers. However, if two values are equal sum will be zero
+#
+# first_num = int(input("x="))
+# second_num = int(input("y="))
+# third_num = int(input("z="))
+#
+# def sum_with_clause(x,y,z):
+#     if x==y:
+#         print(z)
+#     elif z==x:
+#         print(y)
+#     elif y==z:
+#         print(x)
+#     else:
+#         print(x+y+z)
+#
+# sum_with_clause(first_num,second_num,third_num)
+
+# Python program to sum of two given integers. However, if the sum is between 15 to 20 it will return 20.
+#
+# first_num = int(input("x="))
+# second_num = int(input("y="))
+#
+# def logic_sum(x,y):
+#     z = x+y
+#     if z in range(15,20):
+#         print(20)
+#     else:
+#         print(z)
+#
+# logic_sum(first_num, second_num)
+
+#Python program that will return true if the two given integer values are equal or their sum or difference is 5.
+#
+# first_num = int(input("x="))
+# second_num = int(input("y="))
+#
+# def number_combo(x,y):
+#     return x==y or (x+y)==5 or abs(x-y)==5
+#
+#
+# print(number_combo(first_num,second_num))
+
+# Python program to add two objects if both objects are an integer type.
+#
+# first_num = input("x=")
+# second_num = input("y=")
+#
+# def check_type(x,y):
+#     if (isinstance(x,int) and isinstance(y,int)):
+#         return x+y
+#     return "Need to be integers!"
+# print(check_type(3,4))
+# print(check_type(3.2,5))
+# print(check_type('1',1))
+
