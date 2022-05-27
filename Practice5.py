13ea9d59193df97bee9ced37eaea537920c2acd2
Som <94239450+fortheloveofcoding99@users.noreply.github.com>
2022-05-27 07:11:17 +0530

    Add files via upload

---
+++ Practice5.py
@@ -0,0 +1,14 @@
+#
+# import platform
+# import sys
+# import os
+# print(platform.system())
+# print(sys.version)
+# print(sys.platform)
+# print(sys.getwindowsversion())
+# print(sys.winver)
+# print(platform.release())
+# print(os.name)
+#
+# import site
+# print(site.getsitepackages())
