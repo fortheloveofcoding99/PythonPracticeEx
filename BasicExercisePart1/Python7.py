f877775299f4dd31ba52862e86805277bafb54cc
Som <94239450+fortheloveofcoding99@users.noreply.github.com>
2022-05-31 00:24:02 +0530

    Add files via upload

---
+++ BasicExercisePart1/Python7.py
@@ -0,0 +1,50 @@
+
+# # Write a python program to access environment variables.
+
+#import os
+# for data in os.environ:
+#     print(data)
+#     print("-"*10)
+#     print(os.environ[data])
+#     print("="*10)
+
+# Python program to get the current username.
+#
+# import getpass
+#
+# print(getpass.getuser())
+
+#  Python to find local IP addresses using Python's stdlib.
+#
+# import socket
+#
+# print(socket.gethostbyname(socket.gethostname()))
+
+# Python program to get execution time (in seconds) for a Python method.
+#
+# import time
+#
+# lower = int(input("to:"))
+# upper = int(input("from:"))
+#
+#
+# def find_time(lower, upper):
+#     start_time = time.time()
+#     for num in range(lower, upper+1):
+#         if num>1:
+#          for i in range (2, num):
+#             if (num%i)==0:
+#                 break
+#          else:
+#              print(num, end=" ")
+#     end_time = time.time()
+#     print("The time taken for the program is {}".format(end_time-start_time))
+#
+#
+# find_time(lower, upper)
+
+
+
+
+
+
